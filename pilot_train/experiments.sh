a710b77f21f060463df75ca466e0159037226cfd
nimiCurtis <nimicu21@gmail.com>
2024-07-25 13:22:16 +0300

    update

--- pilot_train/experiments.sh
+++ pilot_train/experiments.sh
@@ -1,3 +1,4 @@
-python3 train.py data.context_size=6
+python3 train.py data.context_size=3
+python3 train.py data.context_size=5
 python3 train.py data.context_size=7
 # python3 train.py data.action.max_dist_cat=14 data.distance.max_dist_cat=16
