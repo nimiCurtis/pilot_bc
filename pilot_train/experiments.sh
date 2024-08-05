3f35e003313d533cd83110642f1587bd6e2d32cf
nimiCurtis <nimicu21@gmail.com>
2024-08-05 15:41:26 +0300

    update go2

--- pilot_train/experiments.sh
+++ pilot_train/experiments.sh
@@ -1,9 +1,9 @@
-python3 train.py data.context_size=0
-# python3 train.py data.context_size=1
+python3 train.py training.use_ema=False
+python3 train.py training.use_ema=True
 # python3 train.py data.context_size=2
-python3 train.py data.context_size=3
+# python3 train.py data.context_size=3
 # python3 train.py data.context_size=4
 # python3 train.py data.context_size=5
-python3 train.py data.context_size=6
+# python3 train.py data.context_size=6
 # python3 train.py data.context_size=7
 # python3 train.py data.action.max_dist_cat=14 data.distance.max_dist_cat=16
