e2beecbdfc2148fc9706ded615299a77831be116
nimiCurtis <nimicu21@gmail.com>
2024-10-28 14:16:02 +0200

    prepare experiments

--- pilot_train/experiments.sh
+++ pilot_train/experiments.sh
@@ -1,5 +1,19 @@
-python3 train.py data.context_size=5 data.action_context_size=3 data.pred_horizon=8 data.action_horizon=5
-python3 train.py data.context_size=2 data.action_context_size=2 data.pred_horizon=8 data.action_horizon=5
-python3 train.py data.context_size=5 data.action_context_size=3 data.pred_horizon=16 data.action_horizon=8
-python3 train.py data.context_size=2 data.action_context_size=2 data.pred_horizon=16 data.action_horizon=8
+# python3 train.py data.context_size=0
+# python3 train.py data.context_size=1 
+# python3 train.py data.context_size=2 
 
+# standard normtype
+python3 train.py data.context_size=0 data.distance.min_dist_cat=12  data.distance.max_dist_cat=20 data.action.min_dist_cat=12  data.action.max_dist_cat=20 data.norm_type=standard 
+python3 train.py data.context_size=1 data.distance.min_dist_cat=12  data.distance.max_dist_cat=20 data.action.min_dist_cat=12  data.action.max_dist_cat=20 data.norm_type=standard
+python3 train.py data.context_size=2 data.distance.min_dist_cat=12  data.distance.max_dist_cat=20 data.action.min_dist_cat=12  data.action.max_dist_cat=20 data.norm_type=standard
+python3 train.py data.context_size=0 data.distance.min_dist_cat=24  data.distance.max_dist_cat=40 data.action.min_dist_cat=24  data.action.max_dist_cat=40 data.norm_type=standard
+python3 train.py data.context_size=1 data.distance.min_dist_cat=24  data.distance.max_dist_cat=40 data.action.min_dist_cat=24  data.action.max_dist_cat=40 data.norm_type=standard
+python3 train.py data.context_size=2 data.distance.min_dist_cat=24  data.distance.max_dist_cat=40 data.action.min_dist_cat=24  data.action.max_dist_cat=40 data.norm_type=standard
+
+# maxmin normtype
+python3 train.py data.context_size=0 data.distance.min_dist_cat=12  data.distance.max_dist_cat=20 data.action.min_dist_cat=12  data.action.max_dist_cat=20 data.norm_type=maxmin
+python3 train.py data.context_size=1 data.distance.min_dist_cat=12  data.distance.max_dist_cat=20 data.action.min_dist_cat=12  data.action.max_dist_cat=20 data.norm_type=maxmin 
+python3 train.py data.context_size=2 data.distance.min_dist_cat=12  data.distance.max_dist_cat=20 data.action.min_dist_cat=12  data.action.max_dist_cat=20 data.norm_type=maxmin
+python3 train.py data.context_size=0 data.distance.min_dist_cat=24  data.distance.max_dist_cat=40 data.action.min_dist_cat=24  data.action.max_dist_cat=40 data.norm_type=maxmin
+python3 train.py data.context_size=1 data.distance.min_dist_cat=24  data.distance.max_dist_cat=40 data.action.min_dist_cat=24  data.action.max_dist_cat=40 data.norm_type=maxmin
+python3 train.py data.context_size=2 data.distance.min_dist_cat=24  data.distance.max_dist_cat=40 data.action.min_dist_cat=24  data.action.max_dist_cat=40 data.norm_type=maxmin
